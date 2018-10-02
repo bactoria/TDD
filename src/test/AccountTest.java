9c2efd2381ca90928aaec984f7b2e7ecb7a686b3
bactoria <bactoria@gmail.com>
2018-10-02 19:34:15 +0900

    Refactor:: Add @Before

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,20 +1,26 @@
 package test;
 
 import main.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
 
+    private Account account;
+
+    @Before
+    public void setup() {
+        account = new Account(10000);
+    }
+
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account(10000);
     }
 
     @Test
     public void testGetBalance() throws Exception {
-        Account account = new Account(10000);
         assertEquals("10000원으로 계좌 생성 후 잔고 조회", 10000, account.getBalance());
 
         account = new Account(1000);
@@ -26,14 +32,12 @@ public class AccountTest {
 
     @Test
     public void testDeposit() throws Exception {
-        Account account = new Account(10000);
         account.deposit(1000);
         assertEquals(11000, account.getBalance());
     }
 
     @Test
     public void testWithdraw() throws Exception {
-        Account account = new Account(10000);
         account.withdraw(1000);
         assertEquals(9000, account.getBalance());
     }
