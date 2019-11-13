2d1eddc2f12a6b889bd428aa19de0b4e4b2e216d
bactoria <bactoria@gmail.com>
2019-11-13 22:17:44 +0900

    빨간줄 :: matchesNothingWhenProfileEmpty()

---
+++ 자바와 JUnit을 활용한 실용주의 단위 테스트/src/test/java/me/bactoria/TddProject/iloveyouboss/ProfileTest.java
@@ -0,0 +1,16 @@
+package me.bactoria.TddProject.iloveyouboss;
+
+import org.junit.Test;
+
+/**
+ * @author Bactoria
+ * @since 2019-11-13 [2019.11월.13]
+ */
+
+public class ProfileTest {
+
+    @Test
+    public void matchesNothingWhenProfileEmpty() {
+        new Profile();
+    }
+}
